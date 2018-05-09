4576d3235a937b3fd6b5f568392b139f0af29846
liushuai3 <1432678620@qq.com>
2018-05-09 19:41:38 +0800

    webchat

--- src/main/java/com/chat/controller/TestController.java
+++ src/main/java/com/chat/controller/TestController.java
@@ -4,15 +4,24 @@ import com.chat.pojo.JSONResult;
 import com.chat.pojo.RedisConfig;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TestController {
     @Autowired
     private RedisConfig redisConfig;
-    @RequestMapping("/")
-    public Object test(){
+    //@RequestMapping("/{name}")
+    //@GetMapping("/")
+   /* @RequestMapping(value = "/", method = RequestMethod.GET)
+    public Object test(@RequestParam(value = "name", required = true)String name){
+        if(name!=null&&!"".equals(name)) {
+            name = name + "webchat";
+        }
+        return name;
+    }*/
+    @RequestMapping(value = "/")
+    public Object test1(){
         return "webchat";
     }
 }
